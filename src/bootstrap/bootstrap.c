7fc182a021e4e8c49382e927f6aee11d994a62f1
lg <abbix@riseup.net>
2022-12-24 14:14:02 -0500

    Add support for hermes

--- src/bootstrap/bootstrap.c
+++ src/bootstrap/bootstrap.c
@@ -1,3 +1,5 @@
+#include <bootstrap_srv.h>
+#include <ichor/alloc.h>
 #include <ichor/charon.h>
 #include <ichor/debug.h>
 #include <ichor/elf.h>
@@ -7,37 +9,34 @@
 #include <ichor/syscalls.h>
 #include <stdc-shim/string.h>
 
-typedef struct __attribute__((packed))
-{
-    PortMessageHeader header;
-    int empty;
-    char character;
-    char str[15];
-} MyMessage;
-
 void _start(Charon *charon)
 {
     Port port;
     Task task;
     VmObject obj;
-    MyMessage message;
-    CharonModule *module;
+    CharonModule *module = NULL;
 
     // We can receive and send from/to this port
     port = sys_alloc_port(PORT_RIGHT_RECV | PORT_RIGHT_SEND);
 
     sys_register_common_port(PORT_COMMON_BOOTSTRAP, port);
 
+    ichor_debug("hello, world");
+
     for (size_t i = 0; i < charon->modules.count; i++)
     {
-        ichor_debug("Module: %s", charon->modules.modules[i].name);
-
         if (strncmp(charon->modules.modules[i].name, "/hello.elf", 10) == 0)
         {
             module = &charon->modules.modules[i];
             ichor_debug("Found hello.elf module! Starting task..");
             break;
         }
+    }
+
+    if (!module)
+    {
+        ichor_debug("Failed to find hello.elf module.. hanging");
+        sys_exit(-1);
     }
 
     task = sys_create_task(RIGHT_NULL);
@@ -72,33 +71,30 @@ void _start(Charon *charon)
     {
         ichor_debug("Failed to spawn task.. hanging, error: %d", sys_errno);
         sys_exit(-1);
-        while (true)
-        {
-        }
     }
+
+    ichor_debug("Waiting for messages");
+
+    BootstrapReq msg = {0};
 
     while (true)
     {
-        ichor_wait_for_message(port, sizeof(message), &message.header);
+        ichor_wait_for_message(port, sizeof(msg), &msg.header);
 
-        if (message.header.port_right == PORT_NULL)
+        if (msg.call == BOOTSTRAP_REGISTER_SERVER)
         {
-            continue;
+            ichor_debug("register_server(%s, %d)", msg.requests.register_server.name, msg.header.port_right);
+
+            BootstrapResponse response = {0};
+
+            response.header.dest = msg.header.port_right;
+            response.header.size = sizeof(response);
+
+            response._data.i32_val = 0;
+
+            sys_msg(PORT_SEND, PORT_NULL, -1, &response.header);
         }
-
-        MyMessage message_to_send;
-        message_to_send.header.dest = message.header.port_right;
-        message_to_send.header.type = PORT_MSG_TYPE_DEFAULT;
-        message_to_send.header.size = sizeof(message);
-        message_to_send.character = 'h';
-        message_to_send.empty = 0;
-
-        char *str = "hello lol";
-
-        memcpy(message_to_send.str, str, strlen(str));
-
-        ichor_debug("Got message: %s, sending message: %s", (char *)message.str, str);
-
-        sys_msg(PORT_SEND, PORT_NULL, -1, &message_to_send.header);
     }
+
+    sys_exit(0);
 }
--- src/hello/hello.c
+++ src/hello/hello.c
@@ -1,45 +1,17 @@
-#include "ichor/debug.h"
+#include <bootstrap.h>
+#include <ichor/debug.h>
 #include <ichor/port.h>
 #include <ichor/syscalls.h>
 #include <stdc-shim/string.h>
 
-typedef struct __attribute__((packed))
-{
-    PortMessageHeader header;
-    int empty;
-    char character;
-    char str[15];
-} MyMessage;
-
 void _start()
 {
-    sys_log("Hello I am 'hello'");
-
     Port port = sys_alloc_port(PORT_RIGHT_RECV | PORT_RIGHT_SEND);
 
-    MyMessage message;
-    message.header.dest = sys_get_common_port(PORT_COMMON_BOOTSTRAP);
-    message.header.type = PORT_MSG_TYPE_RIGHT_ONCE;
-    message.header.size = sizeof(message);
-    message.character = 'h';
-    message.empty = 0;
-    message.header.port_right = port;
+    ichor_debug("registering 'hello'");
+    bootstrap_register_server("org.nyx.hello", port);
 
-    char *str = "hello world";
-
-    memcpy(message.str, str, strlen(str));
-
-    ichor_debug("Sending str \"%s\"", str);
-
-    sys_msg(PORT_SEND, PORT_NULL, 0, &message.header);
-
-    int bytes_received = 0;
-    MyMessage received_message;
-    while (!bytes_received)
-    {
-        bytes_received = sys_msg(PORT_RECV, port, sizeof(MyMessage), &received_message.header);
-    }
-    ichor_debug("Received string %s", received_message.str);
+    sys_free_port(port);
 
     sys_exit(0);
 }
